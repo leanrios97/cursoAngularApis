f1ec94e219cfa7462c0539574184cd5fcdfbe29f
Lean Rios <lrios@ESF-PC-04.software-factory.encodesa.internal>
2023-02-10 17:38:47 -0300

    4to commit

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -28,6 +28,9 @@ export class ProductsComponent implements OnInit {
     description: ''
   };
 
+  limit = 10;
+  offset = 0;
+
   constructor(
     private storeService: StoreService,
     private productsService: ProductsService
@@ -36,7 +39,7 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.productsService.getAllProducts()
+    this.productsService.getProductsByPage(10, 0)
     .subscribe(data => {
       this.products = data;
     });
@@ -91,6 +94,14 @@ export class ProductsComponent implements OnInit {
       const productIndex = this.products.findIndex(item => item.id === this.productChosen.id);
       this.products.splice(productIndex, 1);
       this.showProductDetail = false;
+    });
+  }
+
+  loadMore(){
+    this.productsService.getProductsByPage(this.limit, this.offset)
+    .subscribe(data => {
+      this.products = this.products.concat(data);
+      this.offset += this.limit;
     });
   }
 }

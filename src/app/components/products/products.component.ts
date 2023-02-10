e254c5c16914475bd5b5a68446a8b5dd542b5471
Leandro Rios <lean.rios1997@gmail.com>
2023-02-10 14:43:35 -0300

    1er commit

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from '../../models/product.model';
+
+import { StoreService } from '../../services/store.service';
+import { ProductsService } from '../../services/products.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  myShoppingCart: Product[] = [];
+  total = 0;
+  products: Product[] = [];
+  showProductDetail = false;
+  productChosen: Product = {
+    id: '',
+    price: 0,
+    images: [],
+    title: '',
+    category: {
+      id: '',
+      name: '',
+    },
+    description: ''
+  };
+
+  constructor(
+    private storeService: StoreService,
+    private productsService: ProductsService
+  ) {
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
+  ngOnInit(): void {
+    this.productsService.getAllProducts()
+    .subscribe(data => {
+      this.products = data;
+    });
+  }
+
+  onAddToShoppingCart(product: Product) {
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
+  }
+
+  toggleProductoDetail(){
+    this.showProductDetail = !this.showProductDetail;
+  }
+  onShowDetail(id: string){
+    this.productsService.getProduct(id)
+    .subscribe(data => {
+      this.toggleProductoDetail();
+      this.productChosen = data;
+    })
+  }
+
+}

8311e97939fd9eeac6a554c9333110df55a9c167
Lean Rios <lrios@ESF-PC-04.software-factory.encodesa.internal>
2023-02-13 15:00:41 -0300

    5to commit

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import { Product, CreateProductDTO, updateProductDTO } from './../models/product.model';
 
@@ -20,7 +21,8 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', limit);
     }
-    return this.http.get<Product[]>(this.apiUrl, {params});
+    return this.http.get<Product[]>(this.apiUrl, {params})
+    .pipe(retry(3));
   }
 
   getProduct(id: string){
